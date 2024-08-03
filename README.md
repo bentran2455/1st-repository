99f8f98be031977634924506f76b6cf81c9d8b9e
bentran2455 <letscontactben@gmail.com>
2024-08-03 17:33:19 +0700

    holiday app

--- README.md
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-# git-demo 123
-
-# ben-netlify
-=======
-# git-demo
->>>>>>> parent of 30dca1a (hehe)
--- test/script.js
+++
@@ -1,9 +0,0 @@
-var a = function(){ 
-    this.b = 1; }  // undefined
-var b = function(){
-    this.b = new a().b;
-    return 5; // 5
-}
-
-var c = b() + new b();
-
